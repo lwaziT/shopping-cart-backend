17fb97021a7cb48d9b6586a10c74dd6aba56da9c
lwaziT <61688164+lwaziT@users.noreply.github.com>
2025-12-09 15:21:05 +0200

    Setup project files

---
+++ src/test/java/com/example/e_commerce_shopping_cart/ECommerceShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_commerce_shopping_cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
