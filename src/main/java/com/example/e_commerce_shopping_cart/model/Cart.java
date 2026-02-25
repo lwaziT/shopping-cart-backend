d3cfc4d2cba281e588d4e0f6d4fe29d8782714b5
lwaziT <61688164+lwaziT@users.noreply.github.com>
2026-02-25 16:05:30 +0200

    Implement Cart entities:   -Modelled relationships:     - Class Cart.java     - Class CartItem.java

---
+++ src/main/java/com/example/e_commerce_shopping_cart/model/Cart.java
@@ -0,0 +1,27 @@
+package com.example.e_commerce_shopping_cart.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal totalAmount = BigDecimal.ZERO;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CartItem> cartItems;
+}
